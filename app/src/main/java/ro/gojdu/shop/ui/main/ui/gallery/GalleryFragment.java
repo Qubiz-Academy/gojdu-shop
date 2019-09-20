d21e7f2a90a358945ac2522b3443ba1e4a4009b0
andrei044 <andrei.constantin042@gmail.com>
2019-09-20 17:01:57 +0300

    Added ShopItemDetailsActivity

--- app/src/main/java/ro/gojdu/shop/ui/main/ui/gallery/GalleryFragment.java
+++ app/src/main/java/ro/gojdu/shop/ui/main/ui/gallery/GalleryFragment.java
@@ -1,5 +1,6 @@
 package ro.gojdu.shop.ui.main.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import ro.gojdu.shop.R;
+import ro.gojdu.shop.ui.shop.ShopItemDetailsActivity;
 
 public class GalleryFragment extends Fragment {
 
@@ -31,5 +33,17 @@ public class GalleryFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(view.getContext(), ShopItemDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/ro/gojdu/shop/ui/shop/ShopItemDetailsActivity.java
+++ app/src/main/java/ro/gojdu/shop/ui/shop/ShopItemDetailsActivity.java
@@ -3,14 +3,23 @@ package ro.gojdu.shop.ui.shop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import ro.gojdu.shop.R;
 
 public class ShopItemDetailsActivity extends AppCompatActivity {
-
+    private ImageView productImage;
+    private TextView textTitle;
+    private TextView textDetails;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_item_details);
+        productImage=findViewById(R.id.imageProduct);
+        textTitle=findViewById(R.id.textViewTitle);
+        textDetails=findViewById(R.id.textViewDetails);
+        textTitle.setText("1234");
+        textDetails.setText("detalii");
     }
 }
