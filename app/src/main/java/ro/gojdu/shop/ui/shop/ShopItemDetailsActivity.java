679bada6d2dc04d8c4d94792c90f52d1d781b932
andrei044 <andrei.constantin042@gmail.com>
2019-09-20 16:13:35 +0300

    Added ShopItemDetailsActivity

---
+++ app/src/main/java/ro/gojdu/shop/ui/shop/ShopItemDetailsActivity.java
@@ -0,0 +1,16 @@
+package ro.gojdu.shop.ui.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ro.gojdu.shop.R;
+
+public class ShopItemDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_item_details);
+    }
+}
