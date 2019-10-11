d399a13b27b263d19eac1353131fc6c00d7ae8e6
Dan <danutrexburex@gmail.com>
2019-10-11 15:23:54 +0300

    Filter

--- app/src/main/java/ro/gojdu/shop/ui/main/ui/home/Product.java
+++ app/src/main/java/ro/gojdu/shop/ui/main/ui/home/Product.java
@@ -3,6 +3,7 @@ package ro.gojdu.shop.ui.main.ui.home;
 public class Product {
     private String name;
     private String description;
+    private int rates;
 
     public Product(String name, String description) {
         this.name = name;
@@ -23,5 +24,13 @@ public class Product {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public int getRates() {
+        return rates;
+    }
+
+    public void setRates(int rates) {
+        this.rates = rates;
     }
 }
